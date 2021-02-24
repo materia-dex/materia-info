6067d31675e0b9549ad2eb866829a22464addbc3
Ivan Sala <slavni96@gmail.com>
2021-02-24 10:21:36 +0100

    Update queries.js

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -419,8 +419,8 @@ export const PAIR_DAY_DATA_BULK = (pairs, startTimestamp) => {
 }
 
 export const GLOBAL_CHART = gql`
-  query uniswapDayDatas($startTime: Int!, $skip: Int!) {
-    uniswapDayDatas(first: 1000, skip: $skip, where: { date_gt: $startTime }, orderBy: date, orderDirection: asc) {
+  query materiaDayDatas($startTime: Int!, $skip: Int!) {
+    materiaDayDatas(first: 1000, skip: $skip, where: { date_gt: $startTime }, orderBy: date, orderDirection: asc) {
       id
       date
       totalVolumeUSD
